7e9e2c444c553828df774c43c305ba93732d05c5
CMS <ch@rlie.xyz>
2024-02-11 20:16:48 +0000

    feat: decrypt encrypted pastes

---
+++ ui/src/lib/encryption/decrypt.ts
@@ -0,0 +1,15 @@
+export async function decryptPaste(paste: string, password: string) {
+	const pasteBuffer = Uint8Array.from(atob(paste), c => c.charCodeAt(0));
+	const iv = pasteBuffer.slice(0, 12);
+	const encrypted = pasteBuffer.slice(12);
+	const encodedPassword = new TextEncoder().encode(password);
+	const passwordHash = await crypto.subtle.digest('SHA-256', encodedPassword);
+	const key = await crypto.subtle.importKey('raw', passwordHash, {
+		name: 'AES-GCM'
+	}, false, ['decrypt']);
+	const decrypted = await crypto.subtle.decrypt({
+		name: 'AES-GCM',
+		iv
+	}, key, encrypted);
+	return new TextDecoder().decode(decrypted);
+}
--- ui/src/routes/p/[id]/+page.svelte
+++ ui/src/routes/p/[id]/+page.svelte
@@ -2,19 +2,30 @@
 	import type { PageData } from './$types';
 	import dayjs from 'dayjs';
 	import relativeTime from 'dayjs/plugin/relativeTime';
+	import { onMount } from 'svelte';
+	import { decryptPaste } from '$lib/encryption/decrypt';
 
 	export let data: PageData;
 
-	dayjs.extend(relativeTime)
+	dayjs.extend(relativeTime);
 
 	let expires_in: string;
 	const expirationDifference = dayjs(data.paste.expiration).diff(dayjs(), 'minutes');
 
 	if (expirationDifference < 10) {
-		expires_in = "soon";
+		expires_in = 'soon';
 	} else {
 		expires_in = dayjs(data.paste.expiration).fromNow();
 	}
+
+	onMount(async () => {
+		if (data.paste.encrypted) {
+			const password = window.location.hash.substring(1);
+			if (password) {
+				data.paste.content = await decryptPaste(data.paste.content, password);
+			}
+		}
+	});
 </script>
 
 <div>
